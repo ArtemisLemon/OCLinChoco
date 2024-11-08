5b5c2e4cb26330b6a7115b4fff21e417927372a3
Matthew Coyle <matthew@coyle.fr>
2024-11-08 03:00:46 +0100

    AttribTable

---
+++ lib/src/main/java/org/oclinchoco/AttribTable.java
@@ -0,0 +1,30 @@
+package org.oclinchoco;
+import org.chocosolver.solver.*;
+import org.chocosolver.solver.variables.IntVar;
+import org.chocosolver.util.tools.ArrayUtils;
+
+public class AttribTable implements NavTable {
+    int rows,cols;
+    int ub; int lb; int ob;
+
+    Model csp;
+    IntVar[][] matrix;
+    IntVar[] nullptrs;
+    Intvar obvar;
+    
+    public AttribTable(CSP m, int rows, int cols){
+        csp = m.csp;
+        this.rows=rows;this.cols=cols;
+        this.ub=IntVar.MAX_INT_BOUND/2; this.lb=IntVar.MIN_INT_BOUND/2; this.ob=this.lb-1;
+
+        this.matrix = csp.intVarMatrix(rows, cols, ob,ub);
+        this.obvar = csp.intVar(ob);
+        
+        this.nullptrs = new IntVar[cols];
+    }
+
+    public IntVar[] navTable(){
+        for(int i=0;i<cols;i++) nullptrs[i] = obvar;
+        return ArrayUtils.concat(nullptrs, ArrayUtils.flatten(matrix));
+    }
+}
