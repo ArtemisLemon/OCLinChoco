daabff0111911b687312a4a9d22d78396aa7c4f6
Matthew Coyle <matthew@coyle.fr>
2024-11-19 01:42:14 +0100

    added occ_vars to navCSP with improved filtering in mind

--- lib/src/main/java/org/oclinchoco/NavCSP.java
+++ lib/src/main/java/org/oclinchoco/NavCSP.java
@@ -7,18 +7,34 @@ import org.chocosolver.solver.variables.IntVar;
 
 public class NavCSP implements Source, Sequence {
     IntVar[] vars;
+    IntVar[] occ_vars;
+
+    IntVar size;
+    IntVar min, max;
     
     public NavCSP(CSP m, Source src, NavTable table){
         int n = src.srcVars().length;
         int nn = table.cols();
         int nnn = n*nn;
         vars = m.csp.intVarArray(nnn,table.lb(), table.ub());
+        // occ_vars = m.csp.intVarArray(-table.lb()+table.ub()+1, 0,nnn);
+        occ_vars = m.csp.intVarArray(table.ub()+1, 0,nnn);
 
         int k=0;
         for(int i=0;i<n;i++) for(int j=0;j<nn;j++){ 
             IntVar ptr = src.srcVars()[i].mul(nn).add(j).intVar(); // = pointer arithm
             m.csp.element(vars[k++],table.navTable(),ptr,0).post();
         }
+
+        m.csp.globalCardinality(vars,table.domain_values(),occ_vars,true);
+
+
+        // Collection Attribute Constraints
+        size = vars[0].mul(-1).add(vars.length).intVar();
+        min = m.csp.intVar(table.lb(),table.ub());
+        m.csp.min(min,vars);
+        max = m.csp.intVar(table.lb(),table.ub());
+        m.csp.max(max,vars);
     }
 
     //Source
@@ -26,7 +42,7 @@ public class NavCSP implements Source, Sequence {
 
     //Sequence & Collection
     public IntVar first(){return vars[0];} //ToDo AFD Constraints!!!!!
-    public IntVar size(){return vars[0];} //Todo
-    public IntVar max(){return vars[0];} //Todo
-    public IntVar min(){return vars[0];} //Todo
+    public IntVar size(){return size;}
+    public IntVar max(){return max;} //Todo
+    public IntVar min(){return min;} //Todo
 }
