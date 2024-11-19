6e044277a263985db7260d6b7e3063dba4260fbd
Matthew Coyle <matthew@coyle.fr>
2024-11-19 01:25:14 +0100

    added a comment on an alternative interface that would build the collection atributes just in time

--- lib/src/main/java/org/oclinchoco/types/Collection.java
+++ lib/src/main/java/org/oclinchoco/types/Collection.java
@@ -6,3 +6,11 @@ public interface Collection {
     public IntVar max();
     public IntVar min();
 }
+
+// public interface  CollectionOperation {
+//     public IntVar size();
+// }
+// Because the other way, navCSP forexample needs to hold the variable that results from this operation
+// which means all size calls would see the same variable
+// but it also means we are building the information regardless of it's eventual use
+// Probably there's another way, maybe singleton like, first call to size() builds the variable for the navCSP node, and subsequence calls reuses that same variable.
