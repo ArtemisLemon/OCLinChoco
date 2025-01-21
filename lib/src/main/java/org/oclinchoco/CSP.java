1a1c49fdc7c4e8fa85e2b6d04c44738d2e57545e
Matthew Coyle <matthew.coyle@imt-atlantique.fr>
2025-01-21 16:10:54 +0100

    added =0 and <-0 in CSP

--- lib/src/main/java/org/oclinchoco/CSP.java
+++ lib/src/main/java/org/oclinchoco/CSP.java
@@ -18,4 +18,14 @@ public class CSP{
 
     public Model model(){return csp;}
     public IntVar nullptr(){return nullptr;}
+
+
+    //To Move?
+    public void ZeroIFFZero(IntVar x, IntVar y){ //x=0 <-> y=0
+        csp.ifOnlyIf(csp.arithm(x, "=", 0), csp.arithm(y, "=", 0));
+    }
+
+    public void ZeroIFZero(IntVar x, IntVar y){ //x=0 <- y=0
+        csp.ifThen(csp.arithm(y, "=", 0), csp.arithm(x, "=", 0));
+    }
 }
