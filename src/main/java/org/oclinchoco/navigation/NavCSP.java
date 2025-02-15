531ce251b793a6aa6c42a51cb825a8989a189a5b
Matthew Coyle <matthew.coyle@imt-atlantique.fr>
2025-02-15 15:29:56 +0100

    fixed vars domain, finished the switch

--- src/main/java/org/oclinchoco/navigation/NavCSP.java
+++ src/main/java/org/oclinchoco/navigation/NavCSP.java
@@ -1,22 +1,22 @@
 package org.oclinchoco.navigation;
 import org.oclinchoco.CSP;
+import org.oclinchoco.property.AttributeTable;
 import org.oclinchoco.property.ReferenceTable;
 import org.oclinchoco.property.SingleIntTable;
 import org.oclinchoco.source.PtrSource;
 import org.oclinchoco.source.Source;
-
 import org.chocosolver.solver.variables.IntVar;
 
 public class NavCSP implements Source {
     IntVar[] vars;
     NavTable target_table;
 
-    public NavCSP(CSP m, PtrSource src, NavTable table){
+    protected NavCSP(CSP m, PtrSource src, NavTable table){
         target_table=table;
         int n = src.pointers().length;
         int nn = table.cols();
         int nnn = n*nn;
-        vars = m.model().intVarArray(nnn,0, table.ub());
+        vars = m.model().intVarArray(nnn,table.lb(), table.ub());
 
         int k=0;
         for(int i=0;i<n;i++) for(int j=0;j<nn;j++){ 
@@ -28,9 +28,15 @@ public class NavCSP implements Source {
     public static NavCSP makeNavCSP(CSP csp, PtrSource src, NavTable table){
         return switch(table){
             case ReferenceTable refs -> new Nav2Ptr(csp, src, refs);
-            case SingleIntTable att -> new Nav2Int(csp, src, att);
+            case AttributeTable att -> new Nav2Ints(csp, src, att);
+            case SingleIntTable var -> navToSingleInt(csp, src, var);
             default -> throw new UnsupportedOperationException("can't navigate to " + table);
         };
+    }
+
+    private static NavCSP navToSingleInt(CSP csp, PtrSource src, NavTable table){
+        if(src.size()==1) return new Nav2Int(csp, src, table);
+        return new Nav2Ints(csp, src, table);
     }
 
     @Override
